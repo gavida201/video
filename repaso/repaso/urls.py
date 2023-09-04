1fa64597cf7af2c0170a0d51a3a3f68e91bf2a36
gavida201 <gavida201@gmail.com>
2023-09-03 21:27:29 -0300

    primera vista saludo

--- repaso/repaso/urls.py
+++ repaso/repaso/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from repaso.views import saludo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
 ]
+
